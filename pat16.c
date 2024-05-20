a819b4452d705e3d400dca8a39d0d426076dd597
Nikhilk2002 <nikhilmkd1523@gmail.com>
2024-05-20 11:19:44 +0530

    number increment traingle

---
+++ pat16.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int i,j,k=0;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=9;j++)
+        {
+           if(j<=k+i)
+           {
+            printf("%d",j);
+           }
+        }
+        k++;
+        printf("\n");
+    }
+}
